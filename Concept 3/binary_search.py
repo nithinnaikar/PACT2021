4e927edbb239c5423fb2c20ac17324fbb491a0a8
nithinnaikar <62145888+nithinnaikar@users.noreply.github.com>
2022-01-04 20:05:51 -0500

    Add files via upload

---
+++ Concept 3/binary_search.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  4 19:56:14 2022
+
+@author: Nithin
+"""
+
+import math
+
+a = list(map(int, input().split()))
+
+def binary_search(array, element):
+    '''
+    Recursive binary search function used to search for an element in a sorted array.
+
+    Parameters
+    ----------
+    array : one-dimensional sorted sequence of real numbers
+
+    Returns
+    -------
+    Boolean: True if element is in array, False otherwise
+
+    '''
+    low = 0
+    high = len(array)
+    
+    mid = math.floor((low + high) / 2)
+    if array[mid] == element:
+        return True
+    elif array[mid] < element:
+        return binary_search(array[mid+1:high], element)
+    else:
+        return binary_search(array[low:mid-1], element)
+        
